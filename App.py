3b809dc89c7e94a2596fe1e7aae81f541aa70f58
amoreno1413 <amae2019@mymail.pomona.edu>
2023-10-26 18:46:29 -0700

    Added support for Gen 4 cards

---
+++ App.py
@@ -0,0 +1,87 @@
+import os
+
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QComboBox, QCompleter, \
+    QListWidget, QListWidgetItem, QDialog
+import sqlite3
+
+
+class ImageWindow(QDialog):
+    def __init__(self, imgPath, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Card Viewer")
+        self.setGeometry(100, 100, 400, 400)
+        self.label = QLabel()
+        self.label.setPixmap(QPixmap(imgPath))
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Trading App")
+        self.setFixedSize(1000, 1000)
+
+        self.label = QLabel()
+        self.input = QLineEdit()
+        self.resultList = QListWidget()
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+        layout.addWidget(self.resultList)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+        self.conn = sqlite3.connect('data.db')
+        self.cur = self.conn.cursor()
+        query = 'SELECT Name, Card_Set, Price FROM Cards'
+        results = self.cur.execute(query).fetchall()
+
+        names = [f"{result[0]} - {result[1]} - ${result[2]}" for result in results]
+
+        completer = QCompleter(names)
+        self.input.setCompleter(completer)
+        completer.setCaseSensitivity(2)
+
+        completer.activated.connect(self.updateLabel)
+        self.resultList.itemClicked.connect(self.resultItemClicked)
+
+    def openCardView(self, imgPath):
+        img = ImageWindow(imgPath, self)
+        img.exec()
+
+    def updateLabel(self):
+        user_input = self.input.text()
+        price = user_input.split("$")[-1]
+        query = 'SELECT Name, Card_Set, Price FROM Cards ' \
+                'WHERE Price BETWEEN ? * .90 AND ? * 1.1 AND Card_Set NOT LIKE "%Promo%" ' \
+                'ORDER BY Card_Set, Price'
+        res = self.cur.execute(query, (price, price)).fetchall()
+        self.resultList.clear()
+
+        for row in res:
+            name, cardSet, price = row
+            text = f"{name} - {cardSet} - ${price}"
+            imgPath = os.path.join("Images", cardSet, f"{name}.jpg")
+
+            item = QListWidgetItem(text)
+            item.imgPath = imgPath
+            self.resultList.addItem(item)
+
+    def resultItemClicked(self, item):
+        self.openCardView(item.imgPath)
+
+
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
---
+++ README.md
@@ -0,0 +1,22 @@
+<!-- Trading App - Pokémon Card Value Checker -->
+
+# Trading App - Pokémon Card Value Checker
+
+This app is designed to help you determine the trade value of your Pokémon cards. Follow these simple steps:
+
+## How to Use the App
+
+1. Enter the name of your Pokémon card in the search bar.
+2. Hit Enter or select from the suggested cards.
+3. You will receive a list of Pokémon cards whose values are within a range of 10% above or below your card's estimated value.
+4. Click on a card's name to view a photo of the card.
+
+## About the App
+
+This app helps you make informed decisions about the value of your Pokémon cards when trading. It's a handy tool for collectors and traders alike. 
+
+At the moment the card database contains cards from XY-SV (Excluding Promos), older cards will be added in the future!
+
+Happy trading!
+
+![Screenshot](App.png)
